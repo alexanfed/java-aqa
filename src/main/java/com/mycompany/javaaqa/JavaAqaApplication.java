45ada511b067015e09498507097b1aab9d602e73
alexf <letsdevjava@gmail.com>
2023-09-13 18:28:59 -0600

    hello_example

---
+++ src/main/java/com/mycompany/javaaqa/JavaAqaApplication.java
@@ -0,0 +1,15 @@
+package com.mycompany.javaaqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class JavaAqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAqaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mycompany/javaaqa/JavaAqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.javaaqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
